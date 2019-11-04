7c4936c329602b49894374d3dccfb1061a5cf7de
Denis Krovyakov <denis.krovyakov@softomate.com>
2019-11-05 05:55:45 +0700

    yolo

--- src/main/kotlin/core/MachineModels.kt
+++ src/main/kotlin/core/MachineModels.kt
@@ -34,31 +34,3 @@ data class Branch(
         return "(${state}, ${remainingString})"
     }
 }
-
-//public readonly Configuration Previous;
-//public readonly char State;
-//public readonly string RemainingString;
-//public readonly int CharIndex;
-//
-//public Configuration(Configuration previous, char state, string str, int charIndex)
-//{
-//    Previous = previous;
-//    State = state;
-//    CharIndex = charIndex;
-//    RemainingString = charIndex < str.Length ? str.Substring(charIndex) : "λ";
-//}
-//
-//public string ReconstructHistory()
-//{
-//    var stack = new Stack<Configuration>();
-//    for (var c = this; c != null; c = c.Previous)
-//    {
-//        stack.Push(c);
-//    }
-//    return string.Join(" ⊢ ", stack);
-//}
-//
-//public override string ToString()
-//{
-//    return $"({State}, {RemainingString})";
-//}
